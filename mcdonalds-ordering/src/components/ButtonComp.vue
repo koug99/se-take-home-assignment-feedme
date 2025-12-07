1bdbe5efecc0cc431aeca3e2b98776fd035336fb
Koughen Mogan <koughen4hire@gmail.com>
2025-12-07 21:31:49 +0800

    implement ordering system

---
+++ mcdonalds-ordering/src/components/ButtonComp.vue
@@ -0,0 +1,11 @@
+<template>
+	<button @click="$emit('clicked')" class="p-2 border border-orange-400 rounded-md hover:bg-orange-200 cursor-pointer bg-white">
+		<slot></slot>
+	</button>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
